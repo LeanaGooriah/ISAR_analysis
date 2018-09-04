45be638746a7993412ae5c5684a6e8663be13414
Leana Gooriah <42959022+LeanaGooriah@users.noreply.github.com>
2018-09-04 23:27:54 +0200

    Update Sn_values.R

--- R_scripts/Sn_values.R
+++ R_scripts/Sn_values.R
@@ -1,6 +1,13 @@
 
 # Interpolating and extrapolating alpha Sn values
 
+# Get average N values from which Sn will be interpolated or extrapolated from local rarefaction curves
+# load diversity file
+
+m <- round(diversity_indices$N)
+
+##### Interpolation and extrapolation using iNEXT #####
+
 # load transect level data
 
 lizards_abund <- read_csv("lizards_andamans - abund.csv")
@@ -10,17 +17,10 @@ lizards_abund <- read_csv("lizards_andamans - abund.csv")
 t_lizards_abund<- t(lizards_abund)
 t_lizards_abund <- as.data.frame(t_lizards_abund)
 
-
-# Get average N values where Sn will be interpolated or extrapolated from local rarefaction curves
-
-m <- c(1,2,3,4,5,6,9,11,13,15,18,27,36)
-
-##### Interpolation and extrapolation using iNEXT #####
-
 inext_data2 <- iNEXT(t_lizards_abund, q = 0, datatype = "abundance", size = m)
 inext_data2
 
-# Extract iNEXT estimates
+# Extract iNEXT estimates per transect per Site/Island
 
 extrapolated_data2 <- inext_data2$iNextEst
 extrapolated_data2
@@ -28,6 +28,8 @@ ext_dat2 <- do.call("rbind",extrapolated_data2)
 
 #lizards_abund <- read_csv("lizards_andamans - abund.csv")
 div_local$a_Sn <- ext_dat2$qD
+
+# Get average values of Sn per Site
 
 alpha_lizards <- div_local %>%
   group_by(Site) %>%
